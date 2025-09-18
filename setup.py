d8d2cd238ddae8a019313c21db9f1459172fe69b
Oscar Bao <oscarbaoxianhao@gmail.com>
2025-09-18 17:55:11 -0400

    Write drive straight node

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'ros_behaviors_fsm'
+package_name = "ros_behaviors_fsm"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='koroko',
-    maintainer_email='choangbill.le@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="koroko",
+    maintainer_email="choangbill.le@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'emergency_stop = ros_behaviors_fsm.emergency_stop:main'
+        "console_scripts": [
+            "emergency_stop = ros_behaviors_fsm.emergency_stop:main",
+            "drive_straight = ros_behaviors_fsm.drive_straight:main",
         ],
     },
 )
